8b45ec9bebed351547e13b7eedccce29bc143516
Chlove <v4nished@protonmail.com>
2024-08-30 05:42:58 -0400

    e

--- src/components/dom/domElements.ts
+++
@@ -1,3 +0,0 @@
-export const chatInput = document.getElementById('chat-input') as HTMLInputElement;
-export const sendButton = document.getElementById('send-button') as HTMLButtonElement;
-export const chatMessages = document.getElementById('chat-messages') as HTMLDivElement;
